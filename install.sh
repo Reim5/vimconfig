0f4f8589a90bc449a4ca9ea0a73c6abcc65ce31a
Reimart Sarmiento <sarmientoreimart@gmail.com>
2024-06-30 21:19:41 +0800

    FEATURE: Recursive package install Added a feature to the install script wherein it will recursively install the listed packages in the given input list.

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/sh
+# Use this script to install packages
 
-giturl=$1
-packagename=`echo $giturl | awk ++NF FS='.+/' OFS=`
+# Input packagelist is a text file that contains the packages or colors
+packagelist=`cat $1`
 
+# Prepare vim runtime directory -- $HOME/.vim
 if [ ! -d $HOME/.vim ]; then
   mkdir -p $HOME/.vim/pack/dist/start/
 fi
 
-git clone $giturl $HOME/.vim/pack/dist/start/$packagename
+if [ $option -eq 0 ];
+then
+  for url in $packagelist
+  do
+    packagename=`basename $url`
+    git clone $url $HOME/.vim/pack/dist/start/$packagename
+  done
+fi
